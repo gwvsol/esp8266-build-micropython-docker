3b2e3d01c4dedba1d63f158adf37ee31db81acd7
gwvsol <gwvsol@gmail.com>
2019-07-02 00:26:39 +0300

    создание скрипта для очистки SDK и сборки прошивки

--- esp32/make.sh
+++ esp32/make.sh
@@ -1,104 +1,68 @@
 #!/bin/bash
-project=$(pwd)
-dt=$(date +'%Y-%m-%d-%H-%M-%S')
-v="ESP32"
-name="$v-$dt.bin"
-cl_mk=false
-ech=false
-wch=false
+PROJECT='/var/fw'
+DT=$(date +'%Y-%m-%d-%H-%M-%S')
+V="ESP32"
+NAME="$V-$DT.bin"
 export ESPIDF=/var/data/esp-idf
 export PATH=$PATH:/var/data/xtensa-esp32-elf/bin
-sdk='/var/data/micropython/ports/esp32'
-firmware='build/firmware.bin'
+SDK='/var/data/micropython/ports/esp32'
+FIRMVARE='build/firmware.bin'
 
 # Очистка и сборка новой прошивки, если на каком-то шаге ошибка дальше не продолжается работа
-clean_make() {
-cd $sdk
-echo "######################### CLEAN SDK ##############################"
-sleep 3
-make clean
-if [[ $? -ne 0 ]]; then
-    echo "######################## Error Clean! ###########################"
-    exit 1
-else
-    echo "################### Clean Old project OK! ########################"
-    echo "######################## MAKE PROJECT ############################"
-    sleep 3
+# Очистка
+clean_project() {
+    cd $SDK
+    echo "######################### CLEAN SDK $V ##############################"
+    sleep 2
+    make clean
+    if [[ $? -ne 0 ]]; then
+        echo "######################## Error Clean! ###########################"
+        exit 1
+    else
+        echo "################### Clean Old project OK! ########################"
+}
+
+# Сборка новой прошивки
+make_project() {
+    cd $SDK   
+    echo "######################## MAKE PROJECT $V ############################"
+    sleep 2
     make
     if [[ $? -ne 0 ]]; then
         echo "###################### Error Make Firmware! ##########################"
         exit 1
     else
         echo "###################### Make Firmware OK! #########################"
-        cp $firmware $project/$name
+        cp $FIRMVARE $PROJECT/$NAME
         echo "NAME FIRMWARE:"
-        echo "$name"
-        cd $project
-        echo "THE FIRMWARE IS LOCATED:"
-        echo "$(pwd)"
+        echo "$NAME"
         echo "##################### FINISH MAKE FIRMWARE ########################"
     fi
 fi
 }
 
-chip_erase() {
-sleep 1
-echo "##################### ERASE FLASH ESP32 ########################"
-esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
-if [[ $? -ne 0 ]]; then
-    echo "######################## ERROR ERASE FLASH! ###########################"
-    exit 1
-fi
-}
-
-chip_write() {
-sleep 1
-echo "############# UPLOAD THE FIRMWARE INTO THE ESP32 ###############"
-echo "UPLOAD THE FIRMWARE $name"
-esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 $name
-if [[ $? -ne 0 ]]; then
-    echo "######################## ERROR UPLOAD THE FIRMWARE! ###########################"
-    exit 1
-fi
-echo "########################### ALL FINISH ############################"
-}
-
 case $1 in
-     "-om" )
-          cl_mk=true
-          ;;
-     "-mw" )
-          cl_mk=true
-          ech=true
-          wch=true
-          ;;
-     "-ow" )
-          name=$2
-          echo "UPLOAD FIRMWARE $name"
-          ech=true
-          wch=true
-          ;;
-     "--help" )
-          echo "############################################ HELP ###############################################"
-          echo "$0 -om | Только сборка прошивки"
-          echo "$0 -mw | Cборка прошивки, очистка контроллера и запись новой прошивки"
-          echo "$0 -ow | Только очистка контроллера и запись прошивки, необходимо передать имя прошивки"
-          echo "$0 --help | Справка по работе со скриптом"
-          echo "#################################################################################################"
-          ;;
-     *)
-          echo "####################################################################"
-          echo "Для получения информации по работе со скриптом $0 --help"
-          echo "####################################################################"
-          ;;
+    "-c" )
+        clean_project
+        ;;
+    "-m" )
+        make_project
+        ;;
+    "-cm" )
+        clean_project
+        make_project
+        ;;
+    "-h" )
+        echo "#################### HELP ##########################################"
+        echo "$0 -c  | Очистка SDK"
+        echo "$0 -m  | Cборка прошивки"
+        echo "$0 -cm | Очистка SDK и сборка прошивки"
+        echo "$0 -h  | Справка по работе со скриптом"
+        echo "####################################################################"
+        ;;
+    *)
+        echo "####################################################################"
+        echo "Для получения информации по работе со скриптом $0 -h"
+        echo "####################################################################"
+        ;;
 esac
-
-if $cl_mk; then clean_make 
-fi
-
-if $ech; then chip_erase 
-fi
-
-if $wch; then chip_write 
-fi
-
