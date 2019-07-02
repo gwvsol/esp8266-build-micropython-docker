919ae8af39c3e5508256e6680ac9cefd15e6cfce
gwvsol <gwvsol@gmail.com>
2019-07-02 11:37:05 +0300

    обновление скрипта сборки

--- esp8266/make.sh
+++ esp8266/make.sh
@@ -1,57 +1,67 @@
 #!/bin/bash
-project="/var/fw"
-dt=$(date +'%Y-%m-%d-%H-%M-%S')
-v="ESP8266"
-name="$v-$dt.bin"
-cl_mk=false
-ech=false
-wch=false
+PROJECT="/var/fw"
+DT=$(date +'%Y-%m-%d-%H-%M-%S')
+V="ESP8266"
+NAME="$V-$DT.bin"
 export PATH=$PATH:/var/data/esp-open-sdk/xtensa-lx106-elf/bin
-sdk='/var/data/micropython/ports/esp8266'
-firmware='build/firmware-combined.bin'
+SDK='/var/data/micropython/ports/esp8266'
+FIRMWARE='build/firmware-combined.bin'
 
 # Очистка и сборка новой прошивки, если на каком-то шаге ошибка дальше не продолжается работа
-make_fw() {
-cd $sdk
-echo "######################### CLEAN SDK ##############################"
-sleep 3
-make clean
-if [[ $? -ne 0 ]]; then
-    echo "######################## Error Clean! ###########################"
-    exit 1
-else
-    echo "################### Clean Old project OK! ########################"
-    echo "######################## MAKE PROJECT ############################"
-    sleep 3
+# Очистка
+clean_project() {
+    cd $SDK
+    echo "######################### CLEAN SDK $V ##############################"
+    sleep 2
+    make clean
+    if [[ $? -ne 0 ]]; then
+        echo "######################## Error Clean! ###########################"
+        exit 1
+    else
+        echo "################### Clean Old project OK! ########################"
+    fi
+}
+
+# Сборка новой прошивки
+make_project() {
+    cd $SDK   
+    echo "######################## MAKE PROJECT $V ############################"
+    sleep 2
     make
     if [[ $? -ne 0 ]]; then
         echo "###################### Error Make Firmware! ##########################"
         exit 1
     else
         echo "###################### Make Firmware OK! #########################"
-        cp $firmware $project/$name
-        echo "FIRMWARE: $name"
-        #cd $project
-        #echo "THE FIRMWARE IS LOCATED:"
-        #echo "$(pwd)"
+        cp $FIRMVARE $PROJECT/$NAME
+        echo "NAME FIRMWARE:"
+        echo "$NAME"
         echo "##################### FINISH MAKE FIRMWARE ########################"
     fi
-fi
 }
 
 case $1 in
-     "-m" )
-            make_fw
-            ;;
-     "-h" )
-            echo "############################################ HELP ###############################################"
-            echo "$0 -m | Очистка от старых сборок и создание новой прошивки"
-            echo "$0 -h | Справка по работе со скриптом"
-            echo "#################################################################################################"
-          ;;
-     *)
-            echo "####################################################################"
-            echo "Для получения информации по работе со скриптом $0 -h"
-            echo "####################################################################"
-          ;;
-esac 
+    "-c" )
+        clean_project
+        ;;
+    "-m" )
+        make_project
+        ;;
+    "-cm" )
+        clean_project
+        make_project
+        ;;
+    "-h" )
+        echo "#################### HELP ##########################################"
+        echo "$0 -c  | Очистка SDK"
+        echo "$0 -m  | Cборка прошивки"
+        echo "$0 -cm | Очистка SDK и сборка прошивки"
+        echo "$0 -h  | Справка по работе со скриптом"
+        echo "####################################################################"
+        ;;
+    *)
+        echo "####################################################################"
+        echo "Для получения информации по работе со скриптом $0 -h"
+        echo "####################################################################"
+        ;;
+esac
