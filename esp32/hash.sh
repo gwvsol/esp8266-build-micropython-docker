cbfa2028e26fbfe636ae28ea3a3498ed2f12269b
gwvsol <gwvsol@gmail.com>
2020-01-03 12:59:46 +0100

    адаптирован для версии esp-idf v3.3

--- esp32/hash.sh
+++ esp32/hash.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd micropython/ports/esp32
-make | grep hash | awk '{print $4}' > /var/temp/hash
+#make | grep hash | awk '{print $4}' > /var/temp/hash
+make | grep 'hash (v3.3)' | awk '{print $5}' > /var/temp/hash
