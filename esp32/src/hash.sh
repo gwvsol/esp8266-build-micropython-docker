8b072887e88e086ffdcdcc7a552fe1d4658b5b62
Mikhail Fedorov <gwvsol@gmail.com>
2022-01-20 22:12:03 +0300

    удаление файлов для переноса в другой проект

--- esp32/src/hash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd micropython/ports/esp32
-#make | grep hash | awk '{print $4}' > /tmp/hash
-# Имеется версия 4.0 bete, но с ней работает не стабильно
-# Используем пока версию 3.3
-make | grep 'hash (v3.3)' | awk '{print $5}' > /tmp/hash
